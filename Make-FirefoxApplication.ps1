5a3e2693df76a53c765db412d055e165396390bb
Jaakko Luoto <jaakko.luoto@tut.fi>
2017-05-25 13:21:23 +0300

    Added own code: String replace in files, AppDeployToolkit support, Creating SCCM Application

--- Make-FirefoxApplication.ps1
+++ Make-FirefoxApplication.ps1
@@ -1,7 +1,12 @@
+# This is a script for automatically creating a silently deployable installer for Firefox ESR 
+# - With file customizations
+# - AppDeployToolkit support
+# - Application creation in Configuration Manager 
 #
-# This is a script for automatically creating a silently deployable installer with customizations
-# for Firefox ESR
+# Jaakko Luoto
+# jaakko.luoto@tut.fi
 #
+# Based on Make-FirefoxAppSource by:
 # Henri Perämäki
 # henri.w.peramaki@jyu.fi
 # 9.5.2017
@@ -17,126 +22,244 @@
 #	</modifications>  
 # 
 # This will copy (and overwrite) a customized mozilla.cfg to the installer source files.
-# All paths are relative, source being relative to the folder this script is being run and
+# All paths are relative, source being relative to the folder of this script and
 # target to the installer source files root.
-#
 
-# Function to get child processes for a parent process
-Function Find-ChildProcess {
-	param($ID=$PID)
-	Get-WmiObject -Class Win32_Process -Filter "ParentProcessID=$ID"
-}
-
-# Some variables
+# Variables
 $SetupURL = "https://download.mozilla.org/?product=firefox-esr-latest&os=win&lang=en-US"
-$TempFolder = $env:temp
-$SetupFileName = "$TempFolder" + "\firefox-latest-esr-setup.exe"
+$TempDir = "C:\Temp"
+$7z = "C:\Program Files\7-Zip\7z.exe"
+$SetupFileName = "$($TempDir)\firefox-latest-esr-setup.exe"
+$ExtractDir = "$($TempDir)\Make-FF-Temp"
+$TempSetupDir = "$($ExtractDir)\installer"
 $DateStamp = Get-Date -Format "yyMMdd"
 $ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $ModificationFile = "$($ScriptDir)\modifications.xml"
-$OutputFolderRoot = "$($ScriptDir)\Output"
+$OutputDirRoot = "$($TempDir)\Make-FF-Output"
+$ContentDirRoot = "\\servershare\apps\Mozilla\Firefox"
 
-# Loading the customizations xml file
+# If using AppDeployToolkit, setting this to $true will:
+# - Put setup files into subdirectory called Files
+# - Set "Run installation and uninstall program as 32-bit process", needed for ServiceUI.exe compatibility
+# - Increase SCCM installation maximum runtime to 120 minutes to allow AppDeployToolkit UI to stay on screen long enough
+$EnableADT = $false
+
+# SCCM related variables
+# Sitecode
+$CMSite = "CM1"
+# Application folder in SCCM
+$AppFolder = "$($CMSite):\Application\Mozilla\Firefox"
+# Application name prefix
+$AppName = "Mozilla Firefox ESR"
+# Install and uninstall commands
+$InstallCommand = 'setup.exe'
+$UninstallCommand = '"C:\Program Files (x86)\Mozilla Firefox\uninstall\helper.exe" -ms'
+# Installation program visibility (One of Hidden, Maximized, Minimized, Normal)
+$InstVisib = 'Normal'
+# Max installation time in minutes
+$MaxRunTime = 15
+# Estimated installation time in minutes
+$EstRunTime = 5
+
+$TempSetupFilesDir = $TempSetupDir
+
+if ($EnableADT) {
+	$TempSetupFilesDir = "$($TempSetupDir)\Files"
+	$MaxRunTime = 120
+}
+
+If (!(Test-Path $7z)) {
+	Write-Host "7-Zip executable not found at $7z" -Foreground Red
+	Exit
+}
+
+# Load the customizations xml file
 $ModificationXML = [xml](Get-Content $ModificationFile)
 if (!$ModificationXML) {
-	Write-Host "Something went wrong. Could not read $ModificationFile."
+	Write-Host "Something went wrong. Could not read $ModificationFile." -Foreground Red
 	Exit
 }
 $Modifications = $ModificationXML.modifications
 
 
-# Downloading the latest Firefox ESR Setup
+# Download the latest Firefox ESR Setup
 Write-Host "Downloading latest setup..."
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile($SetupURL, $SetupFileName)
 
-# Check that it was really downloaded
+# Check that download was successful
 If (!(Test-Path $SetupFileName)) {
-	Write-Host "Setup file not found at $SetupFileName"
+	Write-Host "Setup file not found in $SetupFileName" -Foreground Red
 	Exit
 }
 
+Write-Host "Extracting files with 7-Zip to $TempSetupFilesDir..." 
+Write-Host "Please ignore any lines about 'ERROR: Data Error'"
+& $7z x -y -t* $SetupFileName -o"$ExtractDir" | Out-Null
+& $7z x -y "$($ExtractDir)\[0]" -o"$TempSetupFilesDir" | Out-Null
 
-# The Firefox ESR Setup is basicly a self-extracting 7z archive. We'll launch it and wait for it to be extracted.
-# Then we can simply copy the actual installer source from the folder where setup.exe is being run.
-
-# Launching the downloaded exe
-Write-Host "Starting the setup..." 
-$7zProcess = Start-Process $SetupFileName -PassThru -WindowStyle Hidden
-Write-Host "Waiting for the setup to extract files..."
-While (!$SetupProcess) {
-	$Temp = Find-ChildProcess -ID $7zProcess.ID
-	If ($Temp.ExecutablePath) {
-		$SetupProcess = $Temp
-	}
+# Check that extraction was successful
+If (!(Test-Path "$($TempSetupFilesDir)\core\firefox.exe")) {
+	Write-Host "Firefox executable not found in $($TempSetupFilesDir)\core" -Foreground Red
+	Exit
 }
 
-# Sleep for 1 second, just to be sure.
-Start-Sleep 1
-
-Write-Host "Child process for Setup.exe found."
-$ExtractedSetupDir = Split-Path -Parent $SetupProcess.ExecutablePath
-
-Write-Host "Checking version info..."
-$FirefoxExeInfo = (Get-ChildItem "$($ExtractedSetupDir)\core\firefox.exe").VersionInfo
+Write-Host "Checking firefox.exe version info..."
+$FirefoxExeInfo = (Get-ChildItem "$($TempSetupFilesDir)\core\firefox.exe").VersionInfo
 $FirefoxFileVersion = New-Object System.Version -ArgumentList @($FirefoxExeInfo.FileMajorPart, $FirefoxExeInfo.FileMinorPart, $FirefoxExeInfo.FileBuildPart, $FirefoxExeInfo.FilePrivatePart)
 $FirefoxProductVersion = $FirefoxExeInfo.ProductVersion
 
 Write-Host "   FileVersion:    $FirefoxFileVersion"
 Write-Host "   ProductVersion: $FirefoxProductVersion"
 
+$AppNameWithVer = "$AppName $FirefoxProductVersion"
 
-$TempSetupDir = "$($ExtractedSetupDir)_autopackage"
-
-Write-Host "Copying setup files to a temporary location:"
-Write-Host "   Source: $ExtractedSetupDir"
-Write-Host "   Target: $TempSetupDir"
-
-Try {
-	Copy-Item $ExtractedSetupDir $TempSetupDir -Recurse
-} catch {
-	Write-Host "Copy failed!"
-	Exit
-}
-
-Write-Host "Copying done."
-
-Write-Host "Killing processes."
-Kill -ID $SetupProcess.ProcessID
-Kill $7zProcess
-Write-Host "   Done"
-
-# Processing modifications found in the modification.xml file
-# This version only supports a action called 'replaceFile', which replaces a original file in the installer source with a customized file
-# Additional actions can be created by writing the functionality for them here
+# Process modifications found in the modification.xml file
+# This version supports following actions: 
+# replaceFile - recursively copies customized files over installer files
+# replaceString - replace string in installer files with support for magic string #Version# for inserting Firefox version
+#
+# Additional actions can be created by writing the functionality for them here.
 Write-Host "Adding modifications to setup files..."
 Foreach ($ReplaceFile in $Modifications.replaceFile) {
 	$ReplaceSource = "$($ScriptDir)\$($ReplaceFile.Source)"
 	$ReplaceTarget = "$($TempSetupDir)\$($ReplaceFile.Target)"
-	Write-Host "   $ReplaceSource -> $ReplaceTarget" -noNewLine
+	Write-Host "   Copy '$ReplaceSource' -> '$ReplaceTarget'" -noNewLine
 	Try {
-		Copy-Item $ReplaceSource $ReplaceTarget -Force
+		Copy-Item $ReplaceSource $ReplaceTarget -Force -Recurse
 		Write-Host "  [OK]" -Foreground Green
-	} catch {
-		Write-Host "  [COPY FAILED]" -Foreground Red
+	} Catch {
+		Write-Host "  [FAILED]" -Foreground Red
 	}
 }
+Foreach ($ReplaceString in $Modifications.replaceString) {
+	$File = "$($TempSetupDir)\$($ReplaceString.File)"
+	$Source = $ReplaceString.Source
+	$Target = $ReplaceString.Target
+	
+	If ($Target -eq "#Version#") {
+		# Magic string #Version# set as target -> replace string with Firefox version
+		$Target = $FirefoxProductVersion
+	}
+	
+	Write-Host "   Replace '$Source' with '$Target' in $File" -noNewLine
+	Try {
+		(Get-Content $File).replace($Source, $Target) | Set-Content $File
+		Write-Host "  [OK]" -Foreground Green
+	} Catch {
+		Write-Host "  [FAILED]" -Foreground Red
+	}
+}	
 
-$OutputFolder = "$($OutputFolderRoot)\Mozilla_Firefox_$($FirefoxProductVersion)esr_$DateStamp"
+$OutputDir = "$($OutputDirRoot)\Mozilla_Firefox_$($FirefoxProductVersion)esr_$DateStamp"
 
-# Making sure that the output folder is unique
+# Make sure that the output folder is unique
 $i = 1
-While (Test-Path $OutputFolder) {
+While (Test-Path $OutputDir) {
 	$i = $i + 1
-	$OutputFolder = "$($OutputFolderRoot)\Mozilla_Firefox_$($FirefoxProductVersion)esr_$($DateStamp)_$i"
+	$OutputDir = "$($OutputDirRoot)\Mozilla_Firefox_$($FirefoxProductVersion)esr_$($DateStamp)_$i"
 }
 
 Write-Host "Copying setup files to the output folder..."
 Write-Host "   Source: $TempSetupDir"
-Write-Host "   Target: $OutputFolder"
+Write-Host "   Target: $OutputDir"
 
-Copy-Item $TempSetupDir $OutputFolder -Recurse
+Copy-Item $TempSetupDir $OutputDir -Recurse
 
-Write-Host "Cleaning up..."
-Remove-Item $ExtractedSetupDir -Force -Recurse
-Remove-Item $TempSetupDir -Force -Recurse
+Write-Host "Cleaning up $ExtractDir..."
+Remove-Item $ExtractDir -Force -Recurse
+
+# Move application source to contentdir
+
+$ContentDir = "$ContentDirRoot\$AppNameWithVer"
+
+# Make sure application title is unique
+# Application is meant to be manually renamed (removing the Generated_xxxxxx string) before creating a deployment
+$AppTitle = "$AppNameWithVer Generated_$DateStamp"
+If ($i -gt 1) {
+	$AppTitle = "$AppTitle`_$i"
+}
+
+Write-Host
+Write-Host "Confirmation for next steps..." -Foreground Yellow
+Write-Host "   - Setup files will be copied to content dir:" -Foreground Yellow
+Write-Host "       Source: '$OutputDir'" -Foreground Yellow
+Write-Host "       Target: '$ContentDir'" -Foreground Yellow
+Write-Host "   - Application object will be created in Configuration Manager:" -Foreground Yellow
+Write-Host "       Name: '$AppTitle'" -Foreground Yellow
+Write-Host "   - Make sure you have modify permissions in target directory and SCCM" -Foreground Yellow
+Pause
+
+Write-Host
+Write-Host "Copying files..."
+
+If (Test-Path "FileSystem::$ContentDir") {
+    Write-Host "Warning: Skipping copy operation because '$ContentDir' already exists." -Foreground Yellow
+} Else {
+    Copy-Item $OutputDir "FileSystem::$ContentDir" -Recurse
+}
+
+# Connect to SCCM
+
+Write-Host "Connecting to Configuration Manager..."
+
+$CMModule = Join-Path $(Split-Path $env:SMS_ADMIN_UI_PATH) ConfigurationManager.psd1
+If (!(Test-Path $CMModule)) {
+	Write-Host "Configuration Manager PowerShell module not found at '$CMModule'. Install Configuration Manager Console." -Foreground Red
+	Exit
+}
+
+# Import SCCM module
+Import-Module $CMModule
+
+# Change working directory to the application folder in SCCM
+$OriginalLocation = Get-Location
+Try {
+	Set-Location $AppFolder -ErrorAction Stop
+} Catch {
+	Write-Host "Error connecting to Configuration Manager site $CMSite and accessing folder $AppFolder" -Foreground Red
+	Exit
+}
+
+Write-Host "Creating Application '$AppTitle'..."
+
+# Create Application object
+Try {
+	$App = New-CMApplication -Name $AppTitle -LocalizedApplicationName $AppNameWithVer -AutoInstall $true -SoftwareVersion $FirefoxProductVersion
+} Catch {
+	Write-Host "Creation of Application failed: $_" -Foreground Red
+	Exit
+}
+
+# Move Application object to SCCM internal folder
+Try {
+	Move-CMObject -FolderPath "$AppFolder" -InputObject $App
+} Catch {
+	Write-Host "Moving Application to folder failed: $_" -Foreground Yellow
+}
+
+# Detection script accepts 32-bit and 64-bit Firefox
+$DetectionScript = "If (Test-Path 'C:\Program Files (x86)\Mozilla Firefox\firefox.exe') {`$version = (Get-ChildItem 'C:\Program Files (x86)\Mozilla Firefox\firefox.exe').VersionInfo.ProductVersion}`nIf (Test-Path 'C:\Program Files\Mozilla Firefox\firefox.exe') {`$version = (Get-ChildItem 'C:\Program Files\Mozilla Firefox\firefox.exe').VersionInfo.ProductVersion}`nIf (`$version -eq [System.Version]'$FirefoxProductVersion') {Write-Host 'Installed'}`nexit 0"
+
+Write-Host "Creating Deployment Type..."
+
+# Create Deployment Type
+Try {
+	# Parameter "Force32Bit" is required for our AppDeployToolkit+ServiceUI.exe setup.
+	# Set it conditionally with splat array
+	$Force32bit = @{'Force32bit'=$EnableADT}
+
+	$DepType = Add-CMScriptDeploymentType -ApplicationName $AppTitle -DeploymentTypeName $AppNameWithVer -ContentLocation "$ContentDirRoot\$AppNameWithVer\" -InstallCommand $InstallCommand -UninstallCommand $UninstallCommand -ScriptLanguage Powershell -Scripttext $DetectionScript -LogonRequirementType WhereOrNotUserLoggedOn -UserInteractionMode $InstVisib -MaximumRunTimeMinutes $MaxRunTime -EstimatedRuntimeMins $EstRunTime -InstallationBehaviorType InstallForSystem @Force32bit
+} Catch {
+	Write-Host "Creation of Deployment Type failed: $_" -Foreground Red
+	Exit
+}
+
+Set-Location $OriginalLocation
+
+Write-Host "Done."
+Write-Host
+Write-Host "Note: Remember manual steps:" -Foreground Yellow
+Write-Host "- If you are happy with the result, rename application and remove Generated_yymmdd string." -Foreground Yellow
+Write-Host "- Add supersedence of previous version in SCCM. Uninstall-checkbox can be left empty." -Foreground Yellow
+Write-Host "- Make a test deployment before deploying to production." -Foreground Yellow
--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # Make-FirefoxApplication
-A PowerShell script to:
-* Automate the creation of a customized Firefox ESR installer package
-* Include support for user interaction with AppDeployToolkit
-* Create a ready-to-deploy Application object in Configuration Manager
 
-Work in progress...
+A PowerShell script to:
+
+* Automate download and creation of a customized Firefox ESR installer package
+* Include support for user interaction with AppDeployToolkit
+* Create a ready-to-deploy Application object in Configuration Manager (SCCM)
+
+This is a fork of <https://github.com/hewipera/Make-FirefoxAppSource>.
+
+Added features:
+
+* Open Firefox setup with 7Zip to avoid having to run the script elevated
+* Support AppDeployToolkit directory structure
+* Make string replacements in files configurable with XML config file
+* Create SCCM Application and Deployment Type
+* Support ServiceUI.exe by running the installation as 32-bit process
+
+The script doesn't enable completely automated workflow. Manual work is required in SCCM to:
+
+* Supercede previous Firefox version (No can do in PowerShell?)
+* Create the deployments (This is possible in PowerShell, but I prefer doing it manually just in case)
+
+## Credits
+
+Contributors:
+
+- hewipera (<https://github.com/hewipera>)
+- Jaakko Luoto (<https://github.com/jaluoto>)
